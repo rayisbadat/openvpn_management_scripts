ac8c44c535627a8b530e447014982cb2a9a9d861
root <root@gdc-ovpn-staging.opensciencedatacloud.org>
2015-07-02 14:36:54 -0500

    creates i /tmp

--- make_zips.sh
+++ make_zips.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 user=${1}
-mkdir $user-gdc; cp /etc/openvpn/ovpn_files/$user-gdc-staging.ovpn /tmp/$user-gdc/; cp /etc/openvpn/ovpn_files_seperated/$user-gdc-staging-seperated.tgz /tmp/$user-gdc/; zip -j $user.zip $user-gdc/*
+cd /tmp
+mkdir $user-gdc; 
+cp /etc/openvpn/ovpn_files/$user-gdc-staging.ovpn /tmp/$user-gdc/; 
+cp /etc/openvpn/ovpn_files_seperated/$user-gdc-staging-seperated.tgz /tmp/$user-gdc/; 
+zip -j $user.zip $user-gdc/*
