4756629a3c3ceb42c4afe78965385b2507fbea18
root <root@gdc-ovpn-staging.opensciencedatacloud.org>
2016-01-22 09:59:19 -0600

    my perl/bash fu was not working, fixed to update passwd properly

--- update_password.sh
+++ update_password.sh
@@ -15,6 +15,8 @@ then
 fi
 
 password_hashed=$( python -c "import bcrypt;print bcrypt.hashpw('$password', bcrypt.gensalt())" )
+#perl -p -i.bak-pwreset -e "s|^($username),(.+)$|\$1,$password_hashed|" $USER_PW_FILE
 
-perl -p -i.bak-pwreset -e "s|^($username),(.+)$|\$1,$password_hashed|" $USER_PW_FILE
+cp $USER_PW_FILE ${USER_PW_FILE}.bak-pwreset
+sed -i "/$username,\\$/d" ../user_passwd.csv && echo "$username,$password_hashed" >> $USER_PW_FILE
 
