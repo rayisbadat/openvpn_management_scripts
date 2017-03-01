78ac4805e64157376d23c65d1d41cf047d89bb6e
root <root@gdc-ovpn-dev.opensciencedatacloud.org>
2017-03-01 15:59:41 +0000

    alerts on system certs as well now

--- expire_cron_alert.sh
+++ expire_cron_alert.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
+
+is_server_cert_expired()
+{
+    OVPN_BASE=/etc/openvpn
+
+    server_crt=$( perl -ne 'm|ca\s+(\S+.crt)| && print "$1\n"' /etc/openvpn/openvpn.conf )
+    crt="${OVPN_BASE}/${server_crt}"
+    is_expired
+    cert_crt=$( perl -ne 'm|cert\s+(\S+.crt)| && print "$1\n"' /etc/openvpn/openvpn.conf )
+    crt="${OVPN_BASE}/${cert_crt}"
+    is_expired
+}
+
+is_expired() {
+
+    if [ -e "$crt" ]
+    then
+
+        date=$(openssl x509 -enddate -noout -in $crt  | perl -ne 'm|notAfter=(.+)| && print "$1\n"')
+        sdate=$(date -d "$date" +%s)
+        today=$(date +%s)
+        cutoff=$(( today + 86400 * 30 ))
+
+        if [ "$sdate" -le "$today" ]
+        then
+            echo $crt,SYSTEM,expired
+        elif [ "$sdate" -le "$cutoff" ]
+        then
+            echo $crt,SYSTEM,expiring_soon
+        fi
+    fi
+
+}
+
+#Check if server cert expired
+is_server_cert_expired
+
+#Check if users expired
 /etc/openvpn/bin/user_status.sh  all | grep expir | ifne mail -s "$(hostname) - VPN Users Expiring Soon" ops@nci-gdc.datacommons.io
+
+
