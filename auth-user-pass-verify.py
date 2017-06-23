c7cbc1d02577487c604cb2fa70bf65f6524cde00
Ray Powell <rpowell1@uchicago.edu>
2017-06-23 15:08:13 +0000

    Will now check if the 'hashed' password is actaully a TOTP secret key and then do a TOTP verfiy instead of password.

--- auth-user-pass-verify.py
+++ auth-user-pass-verify.py
@@ -1,40 +1,57 @@
 #!/usr/bin/env python
-#   Copyright 2015 CDIS
+# Copyright 2015 CDIS
 #   Author: Ray Powell rpowell1@uchicago.edu
-#  This script assumes that openvpn is santizing inputs like it should. Converting to underscores.
-#  It checks the passwd hash table to ensure user supplied password is correct
+#  This script assumes that openvpn is santizing inputs
+#     like it should. Converting to underscores.
+#  It checks the passwd hash table to ensure user supplied
+#     password is correct
 
 import os
 import sys
 import csv
 import bcrypt
+import pyotp
 
 
-#Read in the username and password for  ENV variables openvpn uses
-username=os.environ['username']
-password=os.environ['password']
-passwd_csv_file=os.environ['USER_PW_FILE']
+# Read in the username and password for  ENV variables openvpn uses
+username = os.environ['username']
+password = os.environ['password']
+passwd_csv_file = os.environ['USER_PW_FILE']
+try:
+    totp_window = int(os.environ['totp_window'])
+except KeyError as e:
+    totp_window = 2
 
-#Hash the password
-#passwd_hashed=bcrypt.hashpw('$password', bcrypt.gensalt())
-
-f = open(passwd_csv_file,'r')
+f = open(passwd_csv_file, 'r')
 reader = csv.reader(f)
 for row in reader:
-    if row[0]==username:
-        hashed=row[1]
+    if row[0] == username:
+        hashed = row[1]
         try:
-            if (bcrypt.hashpw(password, hashed) == hashed):
-                f.close()
-                sys.exit(0)
-            #if you uncomment this it only checks the first instance of the username, which is probally the prefered method
-            #else:
-            #    f.close()
-            #    sys.exit(1)
+            # If bcryt hashes detected, check user supplied password hash
+            if (hashed.startswith('$2a$')):
+                if (bcrypt.hashpw(password, hashed) == hashed):
+                    f.close()
+                    sys.exit(0)
+                else:
+                    f.close()
+                    sys.exit(1)
+            # TOTP code detected, verify TOTP tokens match window
+            elif (hashed.startswith('$TOTP$')):
+                secret = hashed.split('$')[2]
+                token = pyotp.TOTP(secret)
+                # if TOTP code within totp_window+1 "tics" allow
+                #    (1 tic = 30sec)
+                if token.verify(otp=password, valid_window=totp_window):
+                    sys.exit(0)
+                else:
+                    sys.exit(1)
+                sys.exit(5)
         except ValueError as e:
+            f.close()
+            sys.exit(1)
             pass
 
+# Fail closed
 f.close()
 sys.exit(1)
-
-        
--- auth-user-pass-verify.sh
+++ auth-user-pass-verify.sh
@@ -6,35 +6,3 @@
 
 source /etc/openvpn/bin/settings.sh
 /etc/openvpn/bin/auth-user-pass-verify.py
-
-#set -e
-#set -u
-#
-#grep "^$username,$password$" $USER_PW_FILE  &> /dev/null && exit 0
-#exit 1
-#
-#
-#hash_passwd() {
-#    password_hashed=$( python -c "import bcrypt;print bcrypt.hashpw('$password', bcrypt.gensalt())" )
-#}
-#
-#compare_hashes() {
-#    result=$(python -c "import bcrypt;password=\"${password}\";hashed=\"${passwd_hash}\"; print( bcrypt.hashpw(password, hashed) == hashed )")
-#    if [ "$result" == "True" ]
-#    then
-#        #True = 0
-#        return 0
-#    fi
-#    #False = 1
-#    return 1
-#}
-#
-#
-#passwd_hash=$(grep -E "^$username,\\\$2a\\\$.+" $USER_PW_FILE | cut -f2 -d',' | head -n1)
-#
-#if compare_hashes
-#then
-#    exit 0
-#else
-#    exit 1
-#fi
