f822b108954fcd41b5b5c24ea205f719903ac0fe
Ray Powell <rpowell1@uchicago.edu>
2017-07-05 20:48:47 +0000

    fully functional, lots of old code to remove

--- send_email.sh
+++ send_email.sh
@@ -39,6 +39,9 @@ create_vpn_user() {
 create_vpn_zip() {
     $VPN_BIN_ROOT/make_zips.sh "$vpn_username"
 }
+set_vpn_totp_secret() {
+    vpn_totp_qrcode=$( $VPN_BIN_ROOT/reset_totp_token.sh "$vpn_username" | tail -n1 )
+}
 vpn_user_exists() {
     grep -E "^$vpn_username," ${VPN_USER_CSV} &>/dev/null
     return $?
@@ -57,6 +60,12 @@ send_welcome_letter_ots() {
     cat $VPN_BIN_ROOT/templates/gdc_creds_template.txt | envsubst | mutt $vpn_email -e "set realname='$EMAIL'"  -s "GDC VPN Configuration Files: $CLOUD_NAME" -a/tmp/$vpn_username.zip $VPN_FILE_ATTACHMENTS
 }
 
+send_welcome_letter_png() {
+
+    #export VPN_CREDS_URL=${vpn_creds_url}
+    export VPN_CREDS_URL=${vpn_totp_qrcode}
+    cat $VPN_BIN_ROOT/templates/gdc_creds_template.txt | envsubst | mutt $vpn_email -e "set realname='$EMAIL'"  -s "GDC VPN Configuration Files: $CLOUD_NAME" -a/tmp/$vpn_username.zip $VPN_FILE_ATTACHMENTS
+}
 
 
 while read line
@@ -89,7 +98,9 @@ do
 
     create_vpn_user
     create_vpn_zip
+    set_vpn_totp_secret
+    send_welcome_letter_png
     #create_ots_site_entry
-    send_welcome_letter_ots
+    #send_welcome_letter_ots
 
 done < ${FILENAME}
